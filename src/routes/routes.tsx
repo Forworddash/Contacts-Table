fb068a0d368c82c64f7e49114f8186b72f6ae1ee
Samuel <samuel.baker@rack-a-tiers.com>
2024-09-15 19:41:46 -0700

    [ADD] added @tanstack/react-router to switch between pages [ADD] added focus_page.tsx for all the details on the contact [FIX] fixed circular import by seperating App function into its own file

---
+++ src/routes/routes.tsx
@@ -0,0 +1,24 @@
+import { createRouter, createRootRoute, createRoute } from '@tanstack/react-router';
+import App from '../App';
+import FocusPage from '../components/focuspage';
+
+
+
+// Define the root route
+const rootRoute = createRootRoute({
+  component: App,
+});
+
+// Define the contact route
+const contactRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/contact/:name',
+  component: FocusPage,
+});
+
+// Create the router instance
+const router = createRouter({
+  routeTree: rootRoute.addChildren([contactRoute]),
+});
+
+export default router;
